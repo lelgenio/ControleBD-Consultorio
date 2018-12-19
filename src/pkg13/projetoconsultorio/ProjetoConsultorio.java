a64f46d7c4960f40ee114708af32bcb0ef746968
leoeugenio <31388299+leoeugenio@users.noreply.github.com>
2018-12-18 22:40:05 -0200

    Add files via upload

---
+++ src/pkg13/projetoconsultorio/ProjetoConsultorio.java
@@ -0,0 +1,12 @@
+package pkg13.projetoconsultorio;
+import telas.telaPrincipal;
+
+public class ProjetoConsultorio {
+    public static void main(String[] args) {
+        
+        
+        telaPrincipal processoPrincipal = new telaPrincipal();
+        processoPrincipal.setVisible(true);
+        
+    }
+}
